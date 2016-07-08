a00442c90bfedfbefe0c4849220ec443c5c32499
Stella Ma <sm873x@gmail.com>
2016-07-08 14:55:15 -0400

    JS - upon click of new list item, para is replaced with new input field

--- inputs.js
+++ inputs.js
@@ -3,34 +3,42 @@
 
     window.todo = ns = ( ns || {} );
 
-    //TODO input form - when 'enter'
+    //Form input - when 'enter'
 
     $('form').on( 'submit', function (e) {
         e.preventDefault();
         var newInput = $('.new-todo').val();
         addNewItem(newInput);
         $('.new-todo').val('');
-
     });
 
+    //Creates new list item
     function addNewItem(newInput) {
         var li = '<li></li>';
-        var article = '<article class="newItem"></article>';
+        var article = '<article class="openItem"></article>';
         var checkButton = '<button class="check"></button>';
         var para = '<p>' + newInput + '</p>';
         var delButton = '<button class="delete">X</button>';
-        $('.items').append(li)
+
+        $('.items')
+            .append(li)
             .find('li:last-child')
                 .append(article)
-                .find('.newItem')
+                .find('.openItem')
                     .append(checkButton + para + delButton);
     }
 
 
-    console.log( $('form') );
+    //Edit item inline
+    $('.items').on('click', '.openItem', editItem);
 
+    function editItem() {
+        console.log('clicked');
+        var input = '<input type="text" class="edit-todo"></input>';
+        $('.openItem p')
+            .replaceWith(input);
+    }
 
-
-
+    
 
 })(window.todo, window.jQuery);
