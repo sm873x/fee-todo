7129c06b4c4dd335a5ed013a3a6afe9daae6edfc
Stella Ma <sm873x@gmail.com>
2016-07-08 12:54:39 -0400

    JS - added new list item with the newInput included inside the paragraph element. HTML - new input adds html elements: li, article, checkbutton, para, delbutton

--- inputs.js
+++ inputs.js
@@ -9,13 +9,21 @@
         e.preventDefault();
         var newInput = $('.new-todo').val();
         addNewItem(newInput);
+        $('.new-todo').val('');
 
     });
 
-    console.log(newInput);
-
     function addNewItem(newInput) {
-        $('.items').append('<li>' + newInput + '</li>');
+        var li = '<li></li>';
+        var article = '<article class="newItem"></article>';
+        var checkButton = '<button class="check"></button>';
+        var para = '<p>' + newInput + '</p>';
+        var delButton = '<button class="delete">X</button>';
+        $('.items').append(li)
+            .find('li:last-child')
+                .append(article)
+                .find('.newItem')
+                    .append(checkButton + para + delButton);
     }
 
 
