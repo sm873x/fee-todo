da7e3b0c927efb6fafa5d5bd62efad1263fc367a
Stella Ma <sm873x@gmail.com>
2016-07-10 16:31:57 -0400

    JS - on click of active, completed and all buttons, fade in/out corresponding completed or incomplete tasks

--- ui.js
+++ ui.js
@@ -3,8 +3,21 @@
 
     window.todo = ns = ( ns || {} );
 
-    //open item count
-    // $('incomplete-items')
-    //     .replaceWith($('.items').length);
-    // console.log(itemList);
+    //Show ALL
+    $('.show-all active').on( 'click', function(e) {
+        $('.items').fadeIn('.completed', '.openItem');
+    });
+
+    //Show ACTIVE only
+    $('.show-active').on( 'click', function(e) {
+        $('.items').fadeOut('.completed');
+        $('.items').fadeIn('.openItem');
+    });
+
+    //Show COMPLETED only
+    $('.show-completed').on( 'click', function(e) {
+        $('.items').fadeOut('.openItem');
+        $('.items').fadeIn('.completed');
+    });
+
 })(window.todo, window.jQuery);
