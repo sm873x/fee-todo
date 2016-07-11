40ee467ac93d9f5f5c16feea0b00d1c7b78594fc
Stella Ma <sm873x@gmail.com>
2016-07-10 23:36:53 -0400

    JS - cleaned up UI comments

--- ui.js
+++ ui.js
@@ -3,21 +3,17 @@
 
     window.todo = ns = ( ns || {} );
 
-    //Show ACTIVE only
     $('.show-active').on( 'click', function(e) {
         $('.openItem').show();
         $('.complete').hide();
     });
 
-    //Show COMPLETED only
     $('.show-completed').on( 'click', function(e) {
         $('.openItem').hide();
         $('.complete').show();
     });
 
-    //Show ALL
     $('.show-all-active').on( 'click', function(e) {
-        //console.log('click');
         $('.openItem').show();
         $('.complete').show();
     });
