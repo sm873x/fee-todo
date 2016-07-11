a42fcc6dd8b3bd89a9c51fafcddafd83d419a2b4
Stella Ma <sm873x@gmail.com>
2016-07-10 21:26:53 -0400

    JS - in ui, when click on active button, only show incomplete list items

--- ui.js
+++ ui.js
@@ -3,21 +3,23 @@
 
     window.todo = ns = ( ns || {} );
 
-    //Show ALL
-    $('.show-all active').on( 'click', function(e) {
-        $('.items').fadeIn('.completed', '.openItem');
-    });
+    // Show ALL
+    // $('.show-all active').on( 'click', function(e) {
+    //     $('.items').fadeIn('.completed', '.openItem');
+    // });
 
     //Show ACTIVE only
     $('.show-active').on( 'click', function(e) {
-        $('.items').fadeOut('.completed');
-        $('.items').fadeIn('.openItem');
+        //$('.openItem').css('opacity', '1');
+        $('.complete').css('display', 'none');
+        // $('.items').fadeOut('.complete');
+        // $('.items').fadeIn('.openItem');
     });
 
     //Show COMPLETED only
-    $('.show-completed').on( 'click', function(e) {
-        $('.items').fadeOut('.openItem');
-        $('.items').fadeIn('.completed');
-    });
+    // $('.show-completed').on( 'click', function(e) {
+    //     // $('.items').fadeOut('.openItem');
+    //     // $('.items').fadeIn('.complete');
+    // });
 
 })(window.todo, window.jQuery);
